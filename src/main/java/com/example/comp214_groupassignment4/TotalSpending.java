70d88c4dc480112003772d567db8ee24b1bdec9e
Ines <leeyfan@gmail.com>
2022-12-08 18:19:21 -0500

    added UI and class template

---
+++ src/main/java/com/example/comp214_groupassignment4/TotalSpending.java
@@ -0,0 +1,22 @@
+package com.example.comp214_groupassignment4;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class TotalSpending {
+
+    @FXML
+    private TextField productID;
+
+    @FXML
+    private Button saveChanges;
+
+    @FXML
+    void OnSaveChanges(ActionEvent event) {
+
+    }
+
+}
+

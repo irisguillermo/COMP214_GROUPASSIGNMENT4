279eec6cc92fc1c70fd732a07011db34f8c4815e
Ines <leeyfan@gmail.com>
2022-12-08 19:31:31 -0500

    added report

---
+++ src/main/java/com/example/comp214_groupassignment4/TotalSpendingData.java
@@ -0,0 +1,24 @@
+package com.example.comp214_groupassignment4;
+
+public class TotalSpendingData {
+
+    private int shopperID;
+    private double totalSpending;
+
+    public int getShopperID() {
+        return shopperID;
+    }
+
+    public void setShopperID(int shopperID) {
+        this.shopperID = shopperID;
+    }
+
+    public double getTotalSpending() {
+        return totalSpending;
+    }
+
+    public void setTotalSpending(double totalSpending) {
+        this.totalSpending = totalSpending;
+    }
+
+}

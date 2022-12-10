3deeeae43e6716038954d5e2cb090262bd095c9e
wingshanip <wip12@my.centennialcollege.ca>
2022-12-09 21:45:40 -0500

    task 1, 2 completed and tested

--- src/main/java/com/example/comp214_groupassignment4/HelloApplication.java
+++ src/main/java/com/example/comp214_groupassignment4/HelloApplication.java
@@ -10,14 +10,9 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-
-
-
-        //FXMLLoader fxmlLoader = new FXMLLoader(TotalSpending.class.getResource("totalSpending.fxml"));
-
-
+//        FXMLLoader fxmlLoader = new FXMLLoader(TotalSpending.class.getResource("totalSpending.fxml"));
+//        FXMLLoader fxmlLoader = new FXMLLoader(EditProductDescription.class.getResource("EditProductDescription.fxml"));
         FXMLLoader fxmlLoader = new FXMLLoader(BBCoffeeMenu.class.getResource("BBCoffeeMenu.fxml"));
-
         Scene scene = new Scene(fxmlLoader.load(), 600, 450);
         stage.setTitle("Welcome to Brewbean's Coffee!");
         stage.setScene(scene);
@@ -25,7 +20,6 @@ public class HelloApplication extends Application {
     }
 
     public static void main(String[] args) {
-
         launch();
     }
 }
---
+++ src/main/java/com/example/comp214_groupassignment4/addNewProduct.java
@@ -0,0 +1,95 @@
+package com.example.comp214_groupassignment4;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import static java.lang.Integer.parseInt;
+
+public class addNewProduct {
+
+    int activeChecked;
+
+    public addNewProduct(){
+
+    }
+
+    @FXML
+    private CheckBox active;
+
+
+    @FXML
+    public void checked(ActionEvent event) {
+        if (active.isSelected()) {
+            activeChecked = 1;
+        }
+        else {
+            activeChecked = 0;
+        }
+    }
+
+    @FXML
+    private TextArea productDescription;
+
+    @FXML
+    private TextField productName, productImage, productPrice;
+
+    @FXML
+    private Button addProduct;
+
+    @FXML
+    public void addProductbtn (ActionEvent event) {
+        Connection connection = null;
+        PreparedStatement statement = null;
+        try {
+            connection = DBUtil.dbConnect();
+            connection.setAutoCommit(false);
+            String query = "{CALL PROD_ADD_SP (?, ?, ?, ?, ?)}";
+            statement = connection.prepareStatement(query);
+            statement.setString(1, productName.getText());
+            statement.setString(2, productDescription.getText());
+            statement.setString(3, productImage.getText());
+            statement.setDouble(4, Double.parseDouble(productPrice.getText()));
+            statement.setInt(5, activeChecked);
+            statement.executeUpdate();
+            int count = statement.executeUpdate();
+            if (count == 1) {
+                this.alert("Success", "New product has been created successfully", Alert.AlertType.INFORMATION);
+            } else {
+                this.alert("Failure", "Some error while creating new product", Alert.AlertType.ERROR);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (null != statement) {
+                try {
+                    statement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (null != connection) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+
+    public void alert(String title, String message, Alert.AlertType alertType) {
+        Alert alert = new Alert(alertType);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+
+}
+
